fba6a5b4b1123590209e2ae5cb958d0df0d5b655
Luan Nico <luanpotter27@gmail.com>
2017-05-22 17:38:11 -0300

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,7 @@ yarn run pack
 
 gcloud compute ssh ambari@$machine -- "ps aux | grep \"[s]torm-true-logger\" | awk '{ print $2; }' | xargs kill -9"
 gcloud compute copy-files dist ambari@$machine:/home/ambari/storm-true-logger
-gcloud compute ssh ambari@$machine -- "./storm-true-logger $topology &"
+ip=`gcloud compute instances list | grep $machine | awk '{ print $(NF-1) }'`
+ssh -i ~/.ssh/google_compute_engine ambari@$ip -C "/home/ambari/storm-true-logger"
+
+# you can ctrl+c after it runs
