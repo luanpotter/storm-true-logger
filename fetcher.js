0174bb16d7609b1f786b6ffc48b07b4fe281b1e8
Luan Nico <luanpotter27@gmail.com>
2017-05-23 14:15:37 -0300

    machine status

--- fetcher.js
+++ fetcher.js
@@ -16,11 +16,16 @@ const ips_p = machines.map(machine => execute(`gcloud compute instances list | g
 Promise.all(ips_p).then(ips => {
 	const logs = ips.map(ip => request('http://' + ip.trim() + ':7472/logs'));
 	Promise.all(logs).then(data => {
+		log('- Machines:');
+		data.map((datum, i) => ips[i] + ' alive and ' + datum.length + ' lines received.').forEach(log);
+
 		const logLines = _.flatten(data.map(el => el.split('\n'))).filter(line => line.match(/\+ \+/g));
 		const filter = regex => logLines.filter(line => line.match(regex)).map(line => extract(line, regex)).map(parseFloat);
 
+		log('- Messages:');
 		log('Produced : ' + filter(/Producing tuple for storm ([0-9]*)$/).length);
 		log('Started : ' + filter(/Started working on ([0-9]*)$/).length);
+		log('Finished : ' + filter(/Finished working on ([0-9]*)$/).length);
 		log('Acked : ' + filter(/Emit and ack ([0-9]*)$/).length);
 		log('Emited : ' + filter(/Producing messages for ActiveMQ ([0-9]*)$/).length);
 	});
